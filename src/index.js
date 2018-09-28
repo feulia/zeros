440af08d82a9c1cc4ede03049c983746e80eefcf
Yulia <43168694+feulia@users.noreply.github.com>
2018-09-28 18:41:56 +0300

    Get my 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+  for (let i = 5 ; i < number; i = i * 5) {
+    result = result + Math.floor(number / i);
+  }
+  return result;
 }
